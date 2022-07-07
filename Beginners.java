4a1f6439612c67fd148b2977d369a3affadd4ce4
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-07 21:54:17 +0530

    Add files via upload

---
+++ Beginners.java
@@ -0,0 +1,18 @@
+package startDemo;
+
+public class Beginners {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=4;int b=6;
+int c=a+b;
+var x="John";
+var z="Mack";
+var s=x+z;
+System.out.println("Hello World");
+System.out.println("Welcome to Spring Suite");
+System.out.println(c);
+System.out.println(s);
+	}
+
+}
