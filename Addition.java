7866a224b3cd0540dfa6ba51c73ea418b1004c5a
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 13:57:05 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+package Beginersassign;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5; int b=2; int c=a+b;
+System.out.println(c);
+	}
+
+}
