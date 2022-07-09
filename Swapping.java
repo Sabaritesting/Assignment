31e0cf6e3dc518d3ebf6b6676d25acc3fe409ea6
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 13:57:59 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,15 @@
+package Beginersassign;
+
+public class Swapping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10; int b=4;
+int temp=a;
+a=b;
+b=temp;
+System.out.println("value of a is"+a);
+System.out.println("value of b is" +b);
+	}
+
+}
