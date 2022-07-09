07aef09fcfb39a8666a43515e57c28d1df2e0c82
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 13:59:16 +0530

    Add files via upload

---
+++ Paragraph.java
@@ -0,0 +1,15 @@
+package Beginersassign;
+
+public class Paragraph {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+var name="Welcome to Spring Suite";
+var begin="Welcome to Java";
+var state="welcome to coding world";
+System.out.println(name);
+System.out.println(begin);
+System.out.println(state);
+	}
+
+}
