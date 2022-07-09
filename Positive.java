107b5dd997e664ffc4cb1a2eebebc982d949bff2
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 13:58:38 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,16 @@
+package Beginersassign;
+
+public class Positive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=4;
+if(a>0) {
+System.out.println(a+ "is a positive number");	
+}
+else {
+System.out.println(a+ "is a negative number");	
+}
+	}
+
+}
