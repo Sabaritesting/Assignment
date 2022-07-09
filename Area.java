da8562444d40190266487dfebc5ab99747e0214c
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 14:01:07 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,12 @@
+package Beginersassign;
+
+public class Area {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=20; int b=14;
+System.out.println(a*a+"is the area of the square");
+System.out.println(a*b+"is the area of the rectangle");
+	}
+
+}
