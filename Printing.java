5af2688133a36dda19b71a97486410196646469b
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 13:56:38 +0530

    Add files via upload

---
+++ Printing.java
@@ -0,0 +1,19 @@
+package Beginersassign;
+
+public class Printing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+var name ="demo";
+float x=10255544;
+double b=0.245;
+boolean s=true;
+System.out.println(a);
+System.out.println(x);
+System.out.println(b);
+System.out.println(s);
+
+	}
+
+}
