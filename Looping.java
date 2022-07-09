f9e343c7c46f3b03bf987cc5597df81907833e0b
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 14:00:16 +0530

    Add files via upload

---
+++ Looping.java
@@ -0,0 +1,19 @@
+package Beginersassign;
+
+public class Looping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=24;
+while(a<50){
+	a++;
+	System.out.println(a);
+}
+{
+
+
+}
+
+	}
+
+}
