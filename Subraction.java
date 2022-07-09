c229eef48b566ffaf50df9658cb1660755b1e3a0
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 14:00:39 +0530

    Add files via upload

---
+++ Subraction.java
@@ -0,0 +1,11 @@
+package Beginersassign;
+
+public class Subraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10; int b=4; int c=a-b;
+System.out.println(c);
+	}
+
+}
