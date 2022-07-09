0daa6574a7130403ea7c74a31f33ac8963c5fa8c
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 13:57:36 +0530

    Add files via upload

---
+++ Mulitplication.java
@@ -0,0 +1,11 @@
+package Beginersassign;
+
+public class Mulitplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float a=10234; float b=12345; float c=a*b;
+System.out.println(c);
+	}
+
+}
