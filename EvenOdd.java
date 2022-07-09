9217bae2b2517ef454779521bc8044466f52dc20
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-09 14:01:38 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,16 @@
+package Beginersassign;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=4;
+if(a%2==0) {
+System.out.println(a+"is even number");	
+}
+else {
+System.out.println(a+"is odd number");	
+}
+	}
+
+}
