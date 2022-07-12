eef77c49b8d0e01c8485b0f09354e8daba4b1e5d
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-12 12:30:10 +0530

    Add files via upload

---
+++ PrintingUser.java
@@ -0,0 +1,42 @@
+package Beginersassign;
+
+import java.util.Scanner;
+
+public class PrintingUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the number");
+int num=sc.nextInt();
+System.out.println(num);
+Scanner sc1=new Scanner(System.in);
+System.out.println("Enter the text");
+var name=sc.next();
+System.out.println(name);
+Scanner sc2=new Scanner(System.in);
+System.out.println("Enter the floting number");
+float x=sc.nextFloat();
+System.out.println(x);
+Scanner sc3=new Scanner(System.in);
+System.out.println("Enter the double number");
+double s=sc.nextDouble();
+System.out.println(s);
+
+Scanner sc4=new Scanner(System.in);
+System.out.println("Enter the condition");
+boolean condit=sc.hasNextByte();
+System.out.println(condit);
+//int a=10;
+//var name ="demo";
+//float x=10255544;
+//double b=0.245;
+//boolean s=true;
+//System.out.println(a);
+//System.out.println(x);
+//System.out.println(b);
+//System.out.println(s);
+
+	}
+
+}
