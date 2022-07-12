129db6cb7a03a7eb5855256ff46067bd2202119b
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-12 12:34:39 +0530

    Add files via upload

---
+++ AreaUser.java
@@ -0,0 +1,23 @@
+package Beginersassign;
+
+import java.util.Scanner;
+
+public class AreaUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the side of  square");
+		int a=sc.nextInt();
+System.out.println(a*a+"is the area of the square");
+Scanner sc1=new Scanner(System.in);
+System.out.println("Enter the length of rectangle");
+int l=sc.nextInt();
+System.out.println(l);
+Scanner sc2=new Scanner(System.in);
+System.out.println("Enter the breadth of rectangle");
+int b=sc.nextInt();
+System.out.println(l*b+ "is the area of the rectangle");
+	}
+
+}
