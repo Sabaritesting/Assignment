08373453d45a4eda23875e0990e473d834f3e18a
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-12 12:33:13 +0530

    Add files via upload

---
+++ MulitplicationUser.java
@@ -0,0 +1,19 @@
+package Beginersassign;
+
+import java.util.Scanner;
+
+public class MulitplicationUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+		float a=sc.nextFloat();
+		Scanner sc1=new Scanner(System.in);
+		System.out.println("Enter the value");
+		float b=sc.nextFloat();
+ float c=a*b;
+System.out.println(c);
+	}
+
+}
