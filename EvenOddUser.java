06d16bd9b50c43a5b5933f663837c5c914f91e48
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-12 12:30:54 +0530

    Add files via upload

---
+++ EvenOddUser.java
@@ -0,0 +1,20 @@
+package Beginersassign;
+
+import java.util.Scanner;
+
+public class EvenOddUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+		int a=sc.nextInt();
+if(a%2==0) {
+System.out.println(a+"is even number");	
+}
+else {
+System.out.println(a+"is odd number");	
+}
+	}
+
+}
