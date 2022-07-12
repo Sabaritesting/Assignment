e7944bece69736e87f99e49ec8ba11e3a27f395f
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-12 12:31:49 +0530

    Add files via upload

---
+++ LoopingUser.java
@@ -0,0 +1,23 @@
+package Beginersassign;
+
+import java.util.Scanner;
+
+public class LoopingUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value");
+		int a=sc.nextInt();
+while(a<50){
+	a++;
+	System.out.println(a);
+}
+{
+
+
+}
+
+	}
+
+}
