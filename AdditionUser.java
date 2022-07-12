9e8d35d7040f4af4b7ae7a0152d41ac869e10260
Sabaritesting <108785595+Sabaritesting@users.noreply.github.com>
2022-07-12 12:29:37 +0530

    Add files via upload

---
+++ AdditionUser.java
@@ -0,0 +1,19 @@
+package Beginersassign;
+
+import java.util.Scanner;
+
+public class AdditionUser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value");
+int a=sc.nextInt();
+Scanner sc1=new Scanner(System.in);
+System.out.println("Enter the value");
+int b=sc.nextInt();
+ int c=a+b;
+System.out.println(c);
+	}
+
+}
